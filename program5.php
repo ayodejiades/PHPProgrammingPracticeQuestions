8c168ab45487bd8c0508b98bcaa8b7ebba4b6af4
ayodejiades <ayodejiadesegun20@gmail.com>
2024-08-19 14:37:56 +0100

    Changes Made

--- program5.php
+++ program5.php
@@ -1,7 +1,31 @@
 <?php
 
-class Numbers{
-    public function __construct($number){
-        
+class Numbers
+{
+    public function __construct($number)
+    {
+        $this->number = $number;
+    }
+
+    public static function checkPrime($number)
+    {
+        $res = true;
+        if ($number <= 1) {
+            return "$number is not prime";
+        }
+        for ($i = 2; $i * $i <= $number; $i++) {
+            if ($number % $i === 0) {
+                $res = false;
+                break;
+            } else {
+                $res = true;
+            }
+        }
+        if (!$res) {
+            return "$number is not prime";
+        }
+        return "$number is prime";
     }
 }
+
+echo Numbers::checkPrime(7);
