9874bbf3c463f56f10d5da40d353213c9f8861c2
ayodejiades <ayodejiadesegun20@gmail.com>
2024-08-19 14:17:02 +0100

    Improved code readability

--- program4.php
+++ program4.php
@@ -1,17 +1,20 @@
 <?php
 
-class IsEvenOrOdd{
+class IsEvenOrOdd
+{
     public static $number;
 
-    public function __construct($number){
+    public function __construct($number)
+    {
         self::$number = $number;
     }
 
-    public static function isEvenOrOdd($number){
-        if($number  % 2 == 0){
-            return "The number".self::$number." is even";
+    public static function isEvenOrOdd($number)
+    {
+        if ($number % 2 == 0) {
+            return "The number" . self::$number . " is even";
         }
-        return "The number".self::$number." is odd";
+        return "The number" . self::$number . " is odd";
     }
 }
 
--- program7.php
+++ program7.php
@@ -1,12 +1,15 @@
 <?php
-class NameDisplay{
+class NameDisplay
+{
     public static $number;
-    public function __construct($number){
+    public function __construct($number)
+    {
         self::$number = $number;
     }
 
-    public static function DisplayName($number){
-        if($number % 3 == 0 ){
+    public static function DisplayName($number)
+    {
+        if ($number % 3 == 0) {
             return "Your Name";
         }
         return "Backend";
