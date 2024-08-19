7766270b830489e6a5e0ab3cb1bf30226ad9e208
ayodejiades <ayodejiadesegun20@gmail.com>
2024-08-19 14:04:00 +0100

    Changes Made

---
+++ program4.php
@@ -0,0 +1,18 @@
+<?php
+
+class IsEvenOrOdd{
+    public static $number;
+
+    public function __construct($number){
+        self::$number = $number;
+    }
+
+    public static function isEvenOrOdd($number){
+        if($number  % 2 == 0){
+            return "The number".self::$number." is even";
+        }
+        return "The number".self::$number." is odd";
+    }
+}
+
+echo IsEvenOrOdd::isEvenOrOdd(4);
