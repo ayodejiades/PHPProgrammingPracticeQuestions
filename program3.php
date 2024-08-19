ffdd94876c397d3a8b66f47f8dbe0301920fb926
ayodejiades <ayodejiadesegun20@gmail.com>
2024-08-19 14:57:36 +0100

    Solved?

--- program3.php
+++ program3.php
@@ -1,6 +1,16 @@
 <?php 
 class Strings{
-    public function __construct(){
-        
+    public function __construct($string){
+        $this->string = $string;
+    }
+
+    public function IsPalindrome($string){
+        for($i = 0; $i < strlen($string) - 1; $i++){
+            $reversedString = strrev($string);
+            if($string [$i] === $reversedString[$i]){
+                return "$string is a palindrome";
+            }
+            return "$string is not a palindrome";
+        }
     }
 }
